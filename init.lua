aed365dacf276c312388f77c987e849d910c6b21
KensSteak <overkill.red99999@gmail.com>
2023-07-05 17:28:41 +0900

    update

--- init.lua
+++ init.lua
@@ -251,6 +251,17 @@ require('lazy').setup({
 },
 
 {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "InsertEnter",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+},
+
+{
   "nvim-telescope/telescope-frecency.nvim",
   config = function()
     require"telescope".load_extension("frecency")
