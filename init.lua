b16b08929ee377a79defb2d76f2d2c0a932b23b5
KensSteak <overkill.red99999@gmail.com>
2023-07-06 17:48:13 +0900

    add  copilot

--- init.lua
+++ init.lua
@@ -91,6 +91,65 @@ require('lazy').setup({
   },
 
   {
+    "github/copilot.vim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    config = function()
+      -- Acceptの引数は、補完候補がないときのキーコード
+      vim.cmd("imap <silent><script><expr> <C-k> copilot#Accept(\"\")")
+      vim.g.copilot_no_tab_map = true
+
+      keymap.set('i', '<C-f>', '<Plug>(copilot-next)', { silent = true })
+      keymap.set('i', '<C-b>', '<Plug>(copilot-previous)', { silent = true })
+    end,
+  },
+
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    config = function()
+      require 'nvim-treesitter.configs'.setup {
+        -- A list of parser names, or "all" (the five listed parsers should always be installed)
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = true,
+
+        -- List of parsers to ignore installing (for "all")
+        ignore_install = {},
+
+        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+        highlight = {
+          enable = true,
+
+          -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+          -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+          -- the name of the parser)
+          -- use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+      }
+    end,
+  },
+
+  {
     'lewis6991/gitsigns.nvim',
     lazy = false,
     config = function()
@@ -714,7 +773,7 @@ require('lazy').setup({
           keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
           keymap.set('n', 'K', vim.lsp.buf.hover, opts)
           keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-          keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          -- keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
           keymap.set('n', '<Leader>wa', vim.lsp.buf.add_workspace_folder, opts)
           keymap.set('n', '<Leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
           keymap.set('n', '<Leader>wl', function()
