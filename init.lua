b2b5d2945b557e277b3e50a0091b3c9590c51a57
KensSteak <overkill.red99999@gmail.com>
2023-07-06 20:27:17 +0900

    fix

--- init.lua
+++ init.lua
@@ -128,7 +128,7 @@ require('lazy').setup({
           -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
           -- the name of the parser)
           -- use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-          disable = function(lang, buf)
+          disable = function(_, buf)
             local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
