74641d11f67a172f80a21eda5881ffbb9dfba9d4
KensSteak <overkill.red99999@gmail.com>
2023-10-24 23:45:25 +0900

    small fix

--- init.lua
+++ init.lua
@@ -139,8 +139,8 @@ require('lazy').setup({
         shade_terminals = false
       })
       vim.cmd('autocmd TermEnter term://*toggleterm#* tnoremap <silent><c-t> <Cmd>exe v:count1 . "ToggleTerm"<CR>')
-      map('n', '<c-t>', '<Cmd>exe v:count1 . "ToggleTerm"<CR>', { noremap = true, silent = true })
-      map('i', '<c-t>', '<esc><Cmd>exe v:count1 . "ToggleTerm"<CR>', { noremap = true, silent = true })
+      map('n', '<A-t>', '<Cmd>exe v:count1 . "ToggleTerm"<CR>', { noremap = true, silent = true })
+      map('i', '<A-t>', '<esc><Cmd>exe v:count1 . "ToggleTerm"<CR>', { noremap = true, silent = true })
 
       function _G.set_terminal_keymaps()
         local opts = { buffer = 0 }
@@ -310,7 +310,7 @@ require('lazy').setup({
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
       }
-      vim.keymap.set("n", "]t", function()
+    vim.keymap.set("n", "]t", function()
         require("todo-comments").jump_next()
       end, { desc = "Next todo comment" })
 
@@ -1016,7 +1016,7 @@ require('lazy').setup({
 
   {
     'hrsh7th/nvim-cmp',
-    event  = 'InsertEnter, CmdlineEnter',
+    event  = {'InsertEnter', 'CmdlineEnter'},
     config = function()
       local has_words_before = function()
         unpack = unpack or table.unpack
